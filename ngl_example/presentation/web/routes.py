6132a98c335464ae9b88ae08abd24041a0837238
brechtvandenberg <brechtvandenberg20@hotmail.com>
2018-10-04 16:33:04 +0200

    implemented textfield, button and made it possible to use custom PDBid

--- ngl_example/presentation/web/routes.py
+++ ngl_example/presentation/web/routes.py
@@ -1,7 +1,19 @@
-from flask import Blueprint, g, render_template
+from flask import Blueprint, g, render_template, request
+from Bio.PDB import PDBList 
 
 bp = Blueprint('web', __name__)
 
-@bp.route('/', methods=['GET'])
+pdbl = PDBList()
+@bp.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method =='POST':
+        if request.form['submit'] == "iets":
+            pdblist = request.form['geneName']
+            for i in pdblist:
+                pdbl.retrieve_pdb_file(i,pdir='PDB')
+
     return render_template('index.html')
+
+@bp.route('/<string:pdb_id>', methods=['GET', 'POST'])
+def getPDB(pdb_id):
+    return render_template('index.html', pdb_id=pdb_id)
