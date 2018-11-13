873cf0a829a3c0e1fedd89ee515f418e61b290ee
brechtvandenberg <brechtvandenberg20@hotmail.com>
2018-11-13 17:02:59 +0100

    did some fuctionality updates and added some comments

--- ngl_example/presentation/web/static/js/dashboard/dashboard.js
+++ ngl_example/presentation/web/static/js/dashboard/dashboard.js
@@ -112,58 +112,134 @@ function residueMutation(pdb_id){
 function parseInput(colorResid){
 	var positionColor = {}
 	var colorResid = $("#colorResid").val(); //  example of data structure A22:#f9d057,A34: #f9d057
-	
+	//console.log(colorResid);
+	/*******
+	First the 'colorResid' array is converted to an Object/dictionary for better separation of color, chain id and positions
+	*******/
 	// separate out each key/value pair
     var parts = colorResid.split(',');
     for(var i = 0; i < parts.length; i++) {
         var p = parts[i];
-        // split Key/Value pair
+        // split key/value pair
         var keyValuePair = p.split(':');
-         
-        // add Key/Value pair to Dictionary object
+        // add key/value pair to dictionary object
         var key = keyValuePair[0];
         var value = keyValuePair[1];
         positionColor[key] = value;
     	};
-    
-    	var colors = Object.values(positionColor);
-    	console.log(colors);
     	
-    
-    console.log("---------------------------positions--------------------------------")
+    var correctedPosCol = Object.keys(positionColor).reduce(function(obj,key){
+    	obj[ positionColor[key] ] = key;
+    	return obj;
+    	},{});
+    //console.log(correctedPosCol);	
     	
-    // the last step the extraction of positions
+    //console.log(positionColor);	
+    colorSort(positionColor);
+    chainidSort(positionColor);
+    return positioncolor;
+};    
+    /******* 
+	Count how many colors are added and then sort by color, every color will get an array of chain id and positions
+	input: ...
+	output: ...
+    *******/    
+function colorSort(positionColor){
+	//output = {color:[A12, A56]}
+	// input: array:[A12, A56, B56] 
+	// output: string 'A and (12, 56) and B (56)'
+	
+    var colors = Object.values(positionColor);
+    //console.log(colors);
+    var map = colors.reduce(function(obj, b) { // this will reduce the same color id's to one 
+    obj[b] = ++obj[b] || 1;
+    	return obj;
+    }, {});
+    var countColor = Object.values(map).length;
+    console.log(Object.keys(map));
+   
+    everyColor = Object.keys(map).forEach(function(element){
+    	console.log(element);
+    	});
+    var sDataArray = MultiDimensionalArray(countColor);
+    console.log(sDataArray);
+    
+    
+    for(var key in positionColor) {
+        if(positionColor[key] === "#e76818" ){
+        	console.log(key);
+        	sDataArray[0].push(key);
+        };
+    }; 
+        console.log(sDataArray[0]);
+
+    
+    var count = 0;
+    //for 
+    
+    
+    
+    
+};   
+function MultiDimensionalArray(iRows,iCols){
+    var i;
+    var j;
+       var a = new Array(iRows);
+       for (i=0; i < iRows; i++){
+           a[i] = new Array(iCols);
+           for (j=0; j < iCols; j++){           
+        	   a[i][j]= "";
+           };
+       };
+       return(a);
+    }; 
+    
+function chainidSort(positionColor){
+    console.log("---------------------------chain id--------------------------------");
     var sortChainPositions = Object.keys(positionColor); 
-    sortChainPositions.sort(); // sorting necessary for the selection    
+    sortChainPositions.sort();  
     var chainPositions = sortChainPositions.toString();
     console.log(chainPositions);
-    
-    var prePositions = ''
-    var positionsRaw = chainPositions.split(','); // extract the positions from chainPositions
-    for(var j = 0; j < positionsRaw.length; j++) {
-        var q = positionsRaw[j];
-        var qq = q.slice(1) +",";
-        prePositions  += qq;         
+    var preChain = '';
+    var chainid = chainPositions.split(','); // extract the chain from chainPositions
+    for(var k = 0; k < chainid.length; k++) {
+        var q = chainid[k];
+        var pp = q.substr(0,1)+",";
+        preChain += pp;
     }
-    var positions = prePositions.slice(0,-1); 
-    console.log(positions);
     
+    var chainid = preChain.slice(0,-1);
+    console.log(chainid);
     
-    
-	return positionColor;
-	//return [chainid, positions, color
-};
+
+    console.log("---------------------------positions--------------------------------");
+};  
+
+
+    /*******
+	Extraction of positions	from the sorting of the colors and chainids
+    *******/
+//    var prePositions = '';
+//    var positionsRaw = chainPositions.split(','); // extract the positions from chainPositions
+//    for(var j = 0; j < positionsRaw.length; j++) {
+//        var q = positionsRaw[j];
+//        var qq = q.slice(1) +"," ;
+//        var pp = q.substr(0,1)+",";
+//        prePositions  += qq;
+//    }
+//    
+//    var positions = "("+ prePositions.slice(0,-1) +")"; 
+//    console.log(positions);
+
 // colors the residues given, receives data from parseInput
-function residueColor(pdb_id, positionColor){
+function residueColor(pdb_id){
 	var colorSele = document.getElementById("colorSelect");
 	colorSele.addEventListener( "click", function(){
-		positionColor = parseInput(positionColor);
-		console.log(positionColor);
-		
 		stage.loadFile("rcsb://"+pdb_id).then(function(col) {
+			parseInput();
 			col.addRepresentation('cartoon',{color:'lightgrey'});
 			col.setSelection('all');
-			var colorRes = col.addRepresentation('licorice',{color:Object.values(positionColor)});
+			var colorRes = col.addRepresentation('licorice',{color:'blue'});
 			colorRes.setSelection("22:A");
 		});
 	});
